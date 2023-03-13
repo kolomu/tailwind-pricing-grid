54586b2f6fe54165a1a6eef20af677220e5b93c3
Kevin Olomu <kevin.olomu@posteo.de>
2023-03-13 06:50:21 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // search for tailwind classes in the html file in the root
+  content: ['./*.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
